10837181465a07fd36d4240bc6bb2c22430226df
Felipe Mello <felipesilva.mello27@gmail.com>
2022-05-06 16:10:10 -0300

    V1

---
+++ IncomeCalculator/Entities/Department.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IncomeCalculator.Entities
+{
+    class Department
+    {
+        public string Name { get; set; }
+
+        public Department()
+        {
+
+        }
+
+        public Department(string name)
+        {
+            Name = name;
+        }
+    }
+
+}
